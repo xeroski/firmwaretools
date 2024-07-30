f25318b4aa3d2d0ccff8ceb4ccbda0849f305d40
xeroski <92430246+xeroski@users.noreply.github.com>
2024-07-30 12:13:32 +0200

    Update parse-uboot-dump.py
    Add conditions if transfer data via UART are damaged, or lines is missing

--- parse-uboot-dump.py
+++ parse-uboot-dump.py
@@ -8,11 +8,26 @@ outfile = sys.argv[2]
 i = open(infile,"r")
 o = open(outfile,"wb")
 
-for line in i.readlines():
-    line = line.strip()
+line_address_prev = 0
+
+for count, line in enumerate(i.readlines()):
+    line = line.strip("\n\t\r")
+    if len(line) != 65:
+    	print("Error! Wrong line length: ", len(line))
+    	print(line)
+    	print("-----------------------------------------------------")
+#    	break
     if re.match(r'^[0-9a-f]{8}:',line):
         line = line.split(":")
-        if len(line) == 2:
+        if len(line) > 1:
+            line_address = int(line[0], base=16)
+            if count > 0:
+            	if (line_address - line_address_prev) != 16:
+            		print("Error! Missing data line")
+            		print("Before line: ", line[0])
+            		print("-----------------------------------------------------")
+#            		break
+            line_address_prev = line_address	
             line = line[1]
             line = line.replace(" ","")[:32]
             data = bytes.fromhex(line)
